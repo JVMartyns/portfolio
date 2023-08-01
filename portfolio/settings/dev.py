99f84c8a5fdbb7a551a9cea72b2444f44041b335
JVMartyns <jvmartyns2015@gmail.com>
2023-07-31 21:33:12 -0300

    change debug to False

--- portfolio/settings/dev.py
+++ portfolio/settings/dev.py
@@ -1,6 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# Generate new key: python -c "import string as s;from secrets import SystemRandom as SR;print(''.join(SR().choices(s.ascii_letters + s.digits + s.punctuation, k=64)));"
 SECRET_KEY = '0K$iK|"bsP4{<.br*itYB$)P8Ad#IIy#QLJfAtuYI/&;Y#(0V<G~N}euKklNc-GG'
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     '.jvmartyns.com.br',
 ]
