7b542db53e2f8e18bf17cba3164e25d87f76c3ec
JVMartyns <jvmartyns2015@gmail.com>
2023-08-01 18:20:56 -0300

    add default acl to s3

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -4,8 +4,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
+    default_acl = 'public-read'
 
 
 class PublicStaticStorage(S3Boto3Storage):
     location = 'static'
-    file_overwrite = False
+    file_overwrite = True
+    default_acl = 'public-read'
