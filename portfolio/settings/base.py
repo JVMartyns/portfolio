8848a2544f44bdcdaabb671c86ad97d6e9fd6632
JVMartyns <jvmartyns2015@gmail.com>
2023-08-01 17:42:50 -0300

    adjust StaticStorage

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -132,9 +132,10 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = 'pt-br'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
