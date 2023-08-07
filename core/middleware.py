5ec80b70060ba65480da445fb6235731c7c3da91
JVMartyns <jvmartyns2015@gmail.com>
2023-08-07 09:52:14 -0300

    remove middleware GoogleAnalyticsMiddleware

--- core/middleware.py
+++ core/middleware.py
@@ -13,20 +13,8 @@ class SocialLinksMiddleware:
         if not social_links:
             social_links = models.SocialLink.objects.all()
             cache.set('social_links', social_links)
-            print('set')
 
         request.social_links = social_links
         response = self.get_response(request)
 
-        return response
-
-
-class GoogleAnalyticsMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        if settings.GA_TRACKING_ID:
-            response.context_data['GA_TRACKING_ID'] = settings.GA_TRACKING_ID
         return response
