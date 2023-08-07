35b22a1dc188a5e3e87bd47fd1557f1cd51dfb75
JVMartyns <jvmartyns2015@gmail.com>
2023-08-06 21:33:52 -0300

    add env var GA_TRACKING_ID

--- core/middleware.py
+++ core/middleware.py
@@ -1,5 +1,6 @@
 from django.core.cache import cache
 from core import models
+from django.conf import settings
 
 
 class SocialLinksMiddleware:
@@ -17,4 +18,15 @@ class SocialLinksMiddleware:
         request.social_links = social_links
         response = self.get_response(request)
 
+        return response
+
+
+class GoogleAnalyticsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if settings.GA_TRACKING_ID:
+            response.context_data['GA_TRACKING_ID'] = settings.GA_TRACKING_ID
         return response
