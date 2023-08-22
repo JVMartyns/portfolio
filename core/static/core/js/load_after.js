6532faf39fab770019f55df4facdc06a1d1a788c
JVMartyns <jvmartyns2015@gmail.com>
2023-08-22 09:48:05 -0300

    get average color

--- core/static/core/js/load_after.js
+++ core/static/core/js/load_after.js
@@ -31,9 +31,10 @@ function showContent() {
 }
 
 function fillCollor(object) {
+    var colorThief = new ColorThief();
     var img = object.getElementsByTagName('img')[0];
-    var rgb = getAverageRGB(img);
-    object.style.background = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.5)`;
+    var corPredominante = colorThief.getPalette(img, 2)[0];
+    object.style.background = `rgba(${corPredominante[0]}, ${corPredominante[1]}, ${corPredominante[2]}, 0.3)`;
 }
 
 // Adiciona um event listener para o evento "load" em cada imagem
